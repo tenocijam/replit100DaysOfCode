fd11496f8174b5a2cb2cf111627d87e55553f531
Sidharth <67367574+tenocijam@users.noreply.github.com>
2022-10-26 13:47:24 +0530

    October 26, 2022

---
+++ Day 3 - Concat what/README.md
@@ -0,0 +1,17 @@
+# Day 3: October 26, 2022
+![Day 3](Day3.jpg)
+
+## Things I learnt today
+
+### Concatenation:
+```python
+name = input("What is your name? ")
+
+print("Hello,", name)
+# INPUT:
+# What is your name? John
+
+# OUTPUT:
+# Hello, John
+```
+It is used to combine text (string) and variables together into a single sentence.
---
+++ Day 3 - Concat what/main.py
@@ -0,0 +1,11 @@
+# Project 3: The Ultimate Wacky Recipe Maker
+
+food = input("Name a food: ")
+plant = input("Name a type of plant: ")
+method = input("Name a method of cooking: ")
+burned = input("What word describes burned food? ")
+item = input("Name a DIY item: ")
+
+print("----------------------")
+print("MENU:")
+print(method, food, "with", burned, plant, "on a bed of", item)
