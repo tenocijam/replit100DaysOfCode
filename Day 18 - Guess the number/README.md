b5dd3441c7975671ffe8141e41f639a4ef0c50e0
Sidharth <67367574+tenocijam@users.noreply.github.com>
2022-11-10 12:08:41 +0530

    November 10, 2022

---
+++ Day 18 - Guess the number/README.md
@@ -0,0 +1,8 @@
+# Day 18: November 10, 2022
+![Day 18](Day18.png)
+
+## Things I learnt today
+
+- Made a number guesser game using the concepts I learnt from the past
+
+[My repl link](https://replit.com/@tenocijam/day18100-days#main.py)
---
+++ Day 18 - Guess the number/main.py
@@ -0,0 +1,25 @@
+# Day 18
+
+print("Guess the number")
+print()
+
+correct_number = 500005
+attempts = 0
+
+while True:
+    print()
+    user_number = int(input("Type a number between 0 and 1,000,000 : "))
+
+    if (user_number < 0):
+        print("Thank you. Bye!")
+        exit()
+    elif (user_number < correct_number):
+        print("Go high")
+        attempts += 1
+    elif (user_number > correct_number):
+        print("Go low")
+        attempts += 1
+    else:
+        attempts += 1
+        print("🎉 You guessed it right!!!")
+        print("It took you", attempts, "attempts to guess the correct number.")
